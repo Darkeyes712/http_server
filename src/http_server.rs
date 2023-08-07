9d6ae260a1741e98a32991c448d53ef9d60954e1
Nikola Kolev <nikolakolev712@gmail.com>
2023-08-07 10:42:56 +0300

    introducing the tokio::spawn logic

--- src/http_server.rs
+++ src/http_server.rs
@@ -2,17 +2,30 @@ use axum::{routing::get, Router};
 use std::net::SocketAddr;
 
 async fn hello_world() -> &'static str {
-    "Hello, this is a simple HTTP server with Axum!"
+    "Maika ti she eva, backa si survura kak si trebe!"
 }
 
-pub async fn run_server() {
+pub async fn run_http_server(port: u16) {
     // Create a new Axum router
     let app = Router::new().route("/", get(hello_world));
 
-    // Set the server address and run the server
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    // Set the server address
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+
+    // Clone the address to print it in the spawned task
+    let print_addr = addr.clone();
+
+    // Spawn the server in a new task
+    tokio::spawn(async move {
+        axum::Server::bind(&addr)
+            .serve(app.into_make_service())
+            .await
+            .unwrap();
+
+        // This will be executed when the server is shut down
+        println!("Server on port {} has been shut down.", print_addr.port());
+    });
+
+    // Print the server's address after starting it
+    println!("Server running at http://{}", addr);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 mod http_server;
-
+use tokio::signal::ctrl_c;
+use tokio::task;
 
 #[tokio::main]
 async fn main() {
-    println!("Server is running...");
-    http_server::run_server().await;
-    println!("Server stopped running.");
+    // Spawning the HTTP server using tokio::spawn()
+    let task = task::spawn(http_server::run_http_server(3000));
+    let task1 = task::spawn(http_server::run_http_server(3001));
+
+    // Wait for the Ctrl+C signal to terminate the server gracefully
+    ctrl_c().await.expect("Failed to listen for Ctrl+C signal");
+
+    // Print a message indicating that the server is shutting down
+    println!("Shutting down the servers...");
+
+    // Wait for the server task to complete gracefully
+    task.await.unwrap();
+
+    // Once the server task completes, the process will terminate gracefully
+    println!("Servers have been shut down.");
 }
